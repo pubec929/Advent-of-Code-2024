d3d6310b4723578eb2b57c7da7e1cf5376187d07
Pubec929 <pubec.308@gmail.com>
2025-06-01 21:24:38 +0200

    finished day 2 part two

--- day 2.py
+++ day 2.py
@@ -1,62 +1,45 @@
 def readFile(fileName):
     with open(fileName, "r", encoding="utf-8") as f:
-        contents = f.readlines()
+        return [list(map(int, line.strip().split())) for line in f]
 
-    matrix = []
-    for row in contents:
-        matrix.append(list(map(int, row.strip().split())))
-    
-    return matrix
-    
-def check(l, r, isIncreasing):
-    ...
+def is_valid(levels):
+    direction = None
+    for i in range(len(levels) - 1):
+        diff = levels[i+1] - levels[i]
 
+        if abs(diff) < 1 or abs(diff) > 3:
+            return False
+
+        if direction is None:
+            direction = diff > 0
+        elif (diff > 0) != direction:
+            return False
+
+    return True
+
+def is_valid_with_removal(levels):
+    for i in range(len(levels)):
+        new_levels = levels[:i] + levels[i+1:]
+        if is_valid(new_levels):
+            return True
+    return False
+
+def partTwo():
+    data = readFile("D:\Paul\Coding Stuff\Python\Advent of Code 2024\day 2-input.txt")
+    safe_count = 0
+    for levels in data:
+        if is_valid(levels) or is_valid_with_removal(levels):
+            safe_count += 1
+    print(safe_count)
 
 def partOne():
     data = readFile("D:\Paul\Coding Stuff\Python\Advent of Code 2024\day 2-input.txt")
-    safeCounter = 0
-    for level in data:
-        isIncreasing = None
-        isSafe = True
-        for i in range(len(level) - 1):
-            difference = level[i] - level[i + 1]
-            if isIncreasing == None:
-                isIncreasing = difference < 0 
-            else:
-                if isIncreasing != (difference < 0):
-                    isSafe = False
-                    break
+    safe_count = 0
+    for levels in data:
+        if is_valid(levels):
+            safe_count += 1
+    print(safe_count)
 
-            if abs(difference) == 0 or abs(difference) >= 4:
-                isSafe = False
-                break 
-
-        if isSafe:
-            safeCounter += 1
-    print(safeCounter)
-def partTwo():
-    data = readFile("D:\Paul\Coding Stuff\Python\Advent of Code 2024\day 2-input.txt")
-    safeCounter = 0
-    for level in data:
-        isIncreasing = None
-        isSafe = True
-        for i in range(len(level) - 1):
-            difference = level[i] - level[i + 1]
-            if isIncreasing == None:
-                isIncreasing = difference < 0 
-            else:
-                if isIncreasing != (difference < 0):
-                    isSafe = False
-                    check(level[i-1], level[i+1], isIncreasing)
-                    break
-
-            if abs(difference) == 0 or abs(difference) >= 4:
-                isSafe = False
-                break 
-
-        if isSafe:
-            safeCounter += 1
-    print(safeCounter)
 
 if __name__ == "__main__":
-    partTwo()
+    partOne()
