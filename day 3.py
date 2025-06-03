eda61f3e35cc8bcfa42641145ae16b54bc1bff63
Pubec929 <pubec.308@gmail.com>
2025-06-03 22:09:42 +0200

    solution day 3 part two

--- day 3.py
+++ day 3.py
@@ -1,9 +1,16 @@
+import re
+
 def readFile(fileName):
     with open(fileName, "r", encoding="utf-8") as f:
         data = f.read()
     return data
 
-def main():
+def partOne():
+    data = open(r"D:\Paul\Coding Stuff\Python\Advent of Code 2024\data\day 3-input.txt", "r", encoding="utf-8").read()
+    matches = re.findall(r"mul\(([0-9]{1,3}),([0-9]{1,3})\)", data)
+    total = sum(int(a) * int(b) for a, b in matches)
+    print(total)
+def partTwo():
     result = 0
     numbers = "0123456789"
     data = readFile("D:\Paul\Coding Stuff\Python\Advent of Code 2024\data\day 3-input.txt")
@@ -12,6 +19,7 @@ def main():
     r = ""
     direction = "l"
     i = 0
+    isEnabled = True
     while i < len(data):   
         if start_sequence:
             if data[i] == "," and l != "" and r == "":
@@ -40,16 +48,17 @@ def main():
                     start_sequence = False
 
                 
-        if data[i:i+4] == "mul(":
+        if data[i:i+4] == "mul(" and isEnabled:
             print("found start sequence")
             start_sequence = True
             i += 4
             continue
-        
+        elif data[i:i+7] == "don't()":
+            isEnabled = False
+        elif data[i:i+4] == "do()":
+            isEnabled = True
         i += 1
     print(result)
-    
-
 
 if __name__ == "__main__":
-    main()
+    partOne()
