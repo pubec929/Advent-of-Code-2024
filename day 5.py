8203cdf6321187f549a0b890c26bdfc9f46f3e7d
Pubec929 <pubec.308@gmail.com>
2025-06-06 22:04:18 +0200

    final day 5 solutions

--- day 5.py
+++ day 5.py
@@ -1,68 +1,64 @@
-# def check(rules, updates):
-#     #print("⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️\n\n")
-#     for row in updates:
-#         safe = True
-#         for index, num in enumerate(row[:len(row)-1]):
-#             if index == len(row) // 2:
-#                 middle = num
-#             if row[index + 1] not in rules[num]:
-#                 safe = False
-#                 break
-        
-#         #print(row, safe)
-
-def main():
-    data = open(r"D:\Paul\Coding Stuff\Python\Advent of Code 2024\data\day 5-input.txt", "r", encoding="utf-8").readlines()
-    global rules
-    rules = {}
+def split_data(data):
     for i, row in enumerate(data):
         if row.strip() == "":
             break
-        l, r = row.strip().split("|")
-        if l in rules:
-            rules[l].append(r)
+    return data[:i], data[i+1:]
+
+
+def format_rules(rules):
+    formated_rules = {}
+    for row in rules:
+        l, r = list(map(lambda n: int(n), row.strip().split("|")))
+        if l in formated_rules:
+            formated_rules[l].append(r)
         else:
-            rules[l] = [r]
-        if r not in rules.keys():
-            rules[r] = []
+            formated_rules[l] = [r]
+        if r not in formated_rules.keys():
+            formated_rules[r] = []
+    return formated_rules
+
+def part_one():
+    data = open(r"D:\Paul\Coding Stuff\Python\Advent of Code 2024\data\day 5-input.txt", "r", encoding="utf-8").readlines()
+    rules, updates = split_data(data)
+    rules = format_rules(rules)
     total = 0
-    # print(rules["99"])
-    # for x in sorted(rules.keys()):
-    #     print(x, sorted(rules[x]))
-    #new_data = []
-    for row in data[i+1:]:
-        row = row.strip().split(",")
-        #print(row == ["68", "11", "75", "89", "42", "49", "62", "86", "24"])
+    for row in updates:
+        row = list(map(lambda n: int(n), row.strip().split(",")))
         safe = True
-        #print(row)
-        for index in range(len(row) - 1):
-            num = row[index]
+        for index, num in enumerate(row[:len(row)-1]):
             if row[index + 1] not in rules[num]:
-                curr = index
                 safe = False
-                row = sort(row)
-        #print(row, safe)
-        #new_data.append(row)
-        if not safe:
-            total += int(row[len(row)//2])
-    #check(rules, new_data[-5:])
+                break
+        if safe:
+            total += row[len(row)//2]
     print(total)
 
-def sort(array):
+def part_two():
+    data = open(r"D:\Paul\Coding Stuff\Python\Advent of Code 2024\data\day 5-input.txt", "r", encoding="utf-8").readlines()
+    rules, updates = split_data(data)
+    rules = format_rules(rules)
+    total = 0
+    for row in updates:
+        row = list(map(lambda n: int(n), row.strip().split(",")))
+        sorted_row = sort(rules, row)
+        if sorted_row != row:
+            total += int(sorted_row[len(sorted_row)//2])
+    print(total)
+    
+
+def sort(rules, array):
     sorted_array = [array[0]]
     for num in array[1:]:
         for i, comp in enumerate(sorted_array):
             if comp in rules[num]:
                 sorted_array.insert(i, num)
                 break
-            if i == len(sorted_array) - 1:
-                sorted_array.append(num)
-                break
+        if i == len(sorted_array) - 1:
+            sorted_array.append(num)
     return sorted_array
     
 
 
 if __name__ == "__main__":
-    main()
-    output = sort(['68', '11', '75', '89', '42', '49', '62', '86', '24'])
+    part_one()
     #check(rules, [output])
